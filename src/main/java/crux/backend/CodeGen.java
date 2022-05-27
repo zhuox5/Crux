187ce65855f0ba966417f020c06a0fc405b3ad4c
Zhuo Xiao <zhuox5@uci.edu>
2022-05-26 23:12:01 -0700

    update proj5

--- src/main/java/crux/backend/CodeGen.java
+++ src/main/java/crux/backend/CodeGen.java
@@ -14,7 +14,7 @@ public final class CodeGen extends InstVisitor {
   private final Program p;
   private final CodePrinter out;
 
-  private HashMap<Variable, Integer> varIndexMap;
+  private HashMap<String, Integer> varIndexMap;
   private int numslots;
   private HashMap<Instruction, String> myLableMap;
 
@@ -130,31 +130,81 @@ public final class CodeGen extends InstVisitor {
 
 
   public void visit(AddressAt i) {
+    printInstructionInfor(i);
+    Symbol src = i.getBase();
+    AddressVar dst = i.getDst();
+    if(i.getOffset() == null){
+      out.bufferCode("movq " + src.getName() + "@GOTPCREL(%rip), %r11");
+      out.bufferCode("movq %r11, " + dst + " (%rbp)");
+    }
+    else{
+      out.bufferCode("imulq $8, %r10");
+      //out.bufferCode("movq " + src.getName() + "@GOTPCREL(%rip), %r10");
+      out.bufferCode("addq %r10, %r11");
+    }
+  }
+
+
+  private int numLocalVar = 1;
+
+
+  public void visit(BinaryOperator i) {
+    //printInstructionInfor(i);
+    String op = i.getOperator().toString();
+    //out.printCode(op);
+    if(op.equals("Add") || op.equals("Sub") || op.equals("Mul") || op.equals("Div")){
+      out.printCode("/* BinaryOperator: "+ op + "*/");
+    }
+    else{
+      op = "No such operator"; //safe checker
+      out.printCode(op);
+    }
+    int stackPosition;
+    if(!varIndexMap.containsKey(i.getLeftOperand().getName())){
+      varIndexMap.put(i.getLeftOperand().getName(), numLocalVar);
+      numLocalVar++;
+      stackPosition = numLocalVar * (-8);
+    }
+    else{
+      stackPosition = varIndexMap.get(i.getLeftOperand().getName()) * (-8);
+    }
+    out.bufferCode("movq "+ stackPosition + "(%rbp), %r10");
 
   }
 
-  public void visit(BinaryOperator i) {}
+  public void visit(CompareInst i) {
+    printInstructionInfor(i);
+  }
 
-  public void visit(CompareInst i) {}
+  public void visit(CopyInst i) {
+    printInstructionInfor(i);
+  }
 
-  public void visit(CopyInst i) {}
+  public void visit(JumpInst i) {
+    printInstructionInfor(i);
+  }
 
-  public void visit(JumpInst i) {}
-
-  public void visit(LoadInst i) {}
+  public void visit(LoadInst i) {
+    printInstructionInfor(i);
+  }
 
   public void visit(NopInst i) {
     printInstructionInfor(i);
   }
 
-  public void visit(StoreInst i) {}
+  public void visit(StoreInst i) {
+    printInstructionInfor(i);
+  }
 
-  public void visit(ReturnInst i) {}
+  public void visit(ReturnInst i) {
+    printInstructionInfor(i);
+  }
 
-  public void visit(CallInst i) {}
+  public void visit(CallInst i) {
+    printInstructionInfor(i);
+  }
 
   public void visit(UnaryNotInst i) {
     printInstructionInfor(i);
-    out.printCode("Unnnnnary");
   }
 }
